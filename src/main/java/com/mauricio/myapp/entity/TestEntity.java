59ded07c288662737ed9d42e7d33fe2f55636477
Mauricio <mdcollante@gmail.com>
2024-10-04 16:53:08 -0300

    Nombre del Proyecto: my-app. Descripcion: version final del proyecto my-app

---
+++ src/main/java/com/mauricio/myapp/entity/TestEntity.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.mauricio.myapp.entity;
+
+/**
+ * @author Mauricio Collante 
+ */
+public class TestEntity {
+	/**
+	*Propiedad de prueba para la muestra de compilacion del archivo testentety	
+	**/
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+
+}
